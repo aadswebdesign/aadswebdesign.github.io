2965a3015667b1250245172b53a89cf34d5c9d94
Aad <aad.pouw@gmail.com>
2023-02-01 18:02:12 +0100

    added some predifined html create functions

--- assets/scripts/htmlManager/htmlElements.js
+++ assets/scripts/htmlManager/htmlElements.js
@@ -1,2 +1,99 @@
 /** htmlManager/htmlElements.js */
 import {createHtmlHelper} from 'htmlHelper.js';
+export const article = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('article', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const aside = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('aside', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const audio = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('audio', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const button = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('button', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const canvas = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('canvas', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const datalist = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('datalist', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const details = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('details', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const div = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('div', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const embed = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('embed', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const footer = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('footer', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h1 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h1', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h2 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h2', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h3 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h3', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h4 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h4', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h5 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h5', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const h6 = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('h6', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const header = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('header', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const icon = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('i', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+
+export const img = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('img', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const li = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('li', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+
+export const nav = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('nav', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+
+export const output = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('output', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+
+export const paragraph = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('p', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const progress = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('progress', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const section = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('section', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const small = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('small', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const span = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('span', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const title = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('title', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const ol = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('ol', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const ul = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('ul', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
+export const video = async (_class, _id, _atts, _content, _template,_parent, _replace, log = false) =>{
+	await createElemHelper('video', _class, _id, _atts, _content, _template,_parent, _replace, log);
+};
