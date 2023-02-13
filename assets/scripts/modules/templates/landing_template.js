587bdc3cce0864b6e3fb0fbd4c8f3c4547ffdeec
Aad <aad.pouw@gmail.com>
2023-02-13 05:34:04 +0100

    another attempt to fix an error
    It's error I don't have on my localhost but just here on gh pages. Makes it a bit hard to solve!

--- assets/scripts/modules/templates/landing_template.js
+++ assets/scripts/modules/templates/landing_template.js
@@ -6,10 +6,7 @@ export async function landingTemplate(){
 	return await `<div class='svg-placeholder landing-template relative'></div>`;
 }
 export const landingTemplateActions = async ()=>{
-	const landing_template = await FT.elQuery('.landing-template');
-	if(landing_template){
-		return await landingTemplate().then(()=>{
-			mediaHandler('(orientation: portrait)',svgLandingPagePortrait,svgLandingPageLandscape,true);
-		});
-	}
+	return await landingTemplate().then(()=>{
+		mediaHandler('(orientation: portrait)',svgLandingPagePortrait,svgLandingPageLandscape,true);
+	});
 };
--- assets/scripts/modules/views/main_view.js
+++ assets/scripts/modules/views/main_view.js
@@ -4,5 +4,8 @@ import * as HE from './../../htmlManager/htmlElements.js';
 import{landingTemplate,landingTemplateActions} from './../templates/landing_template.js';
 export async function mainView(){
 	await HE.div('block page relative', null, null, await landingTemplate(),true, 'main');
-	await landingTemplateActions();
+	const landing_template = await FT.elQuery('.landing-template');
+	if(landing_template){
+		await landingTemplateActions();
+	}
 }
