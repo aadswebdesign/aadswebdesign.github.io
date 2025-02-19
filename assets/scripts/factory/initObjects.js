844460c029ddfbf1545fbcad58d8cc277af6f645
aadswebdesign <aad.pouw@gmail.com>
2025-02-19 12:33:17 +0100

    added some magic

--- assets/scripts/factory/initObjects.js
+++ assets/scripts/factory/initObjects.js
@@ -14,6 +14,9 @@ export const init_objects = async (elems)=>{
 	const {global,vvp,body,wrap_ctn} = elems;
 	const sub_map = new Map([['sub_obj',{
 		global,vvp,body,wrap_ctn,
+		top_elem: await FT.elQuery('section.top', false, self.wrap_ctn), 
+		main_elem: await FT.elQuery('main', false, self.wrap_ctn), 
+		footer_elem: await FT.elQuery('footer', false, self.wrap_ctn), 
 		//todo
 	}]]);
 	return sub_map.get('sub_obj');
