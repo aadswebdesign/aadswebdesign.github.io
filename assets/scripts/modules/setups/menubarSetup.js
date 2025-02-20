b68654fd8187110ae4cd6a9fd4e4a4231d634122
aadswebdesign <aad.pouw@gmail.com>
2025-02-20 06:58:12 +0100

    added 2 empty js files and took some out of other js file

---
+++ assets/scripts/modules/setups/menubarSetup.js
@@ -0,0 +1,3 @@
+/**Github  modules/setups/menubarSetup.js */
+import * as FT from './../../factory/functions.js';
+import * as HL from './../../factory/handlers.js';
---
+++ assets/scripts/modules/setups/toolbarSetup.js
@@ -0,0 +1,3 @@
+/**Admin  modules/setups/toolbarSetup.js */
+import * as FT from './../../factory/functions.js';
+import * as HL from './../../factory/handlers.js';
