01427c575c2e1423fa88d9c0018f7acd6f3f0db7
aadswebdesign <aad.pouw@gmail.com>
2025-02-22 14:11:48 +0100

    typos are part of it

--- assets/scripts/modules/setups/locationsSetup.js
+++ assets/scripts/modules/setups/locationsSetup.js
@@ -4,7 +4,7 @@ export const locations_setup = async (elems)=>{
 	//console.log('locations_setup');
 	console.table({'menu_items': elems});
 	const {location_base} = elems;
-	const [menu_item1,] = elems.menu_items_one;
+	const [menu_item1] = elems.menu_items_one;
 	const [menu_item_home] =  elems.menu_item_home;
 	console.log('menu_item_home = ',menu_item_home);
 	const path = location.pathname;
