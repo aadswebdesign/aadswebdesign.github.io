78da47c17f39bc9618f4c1725791bf686d2741df
aadswebdesign <aad.pouw@gmail.com>
2025-02-26 09:18:48 +0100

    some more adjustments

--- assets/scripts/modules/setups/defaultsSetup.js
+++ assets/scripts/modules/setups/defaultsSetup.js
@@ -4,14 +4,14 @@ import * as MBOT from './../templates/mbBlockOneTpl.js';
 import {demo_home_tpl} from './../templates/demoHomeTpl.js';
 
 export const defaults_setup = async (elems)=>{
-	console.table({'defaults_setup': elems});
+	//console.table({'defaults_setup': elems});
 	const {target_ctn} = elems;	
 	const loc_path = location.pathname;
 	const loc_hash = location.hash;
 	const loc = `${loc_path}${loc_hash}`;
-	console.log('defaults path1 = ',loc_path);
-	console.log('defaults hash1 = ',loc_hash);
-	console.log('defaults loc1 = ',loc);
+	//console.log('defaults path1 = ',loc_path);
+	//console.log('defaults hash1 = ',loc_hash);
+	//console.log('defaults loc1 = ',loc);
 	switch(loc){
 		case `/#home`:{
 			const content_home = await demo_home_tpl();
@@ -23,10 +23,10 @@ export const defaults_setup = async (elems)=>{
 			await FT.setContent(target_ctn,content1);
 		}
 		break;
-		//default:{
-			//const content_home = await demo_home_tpl();
-			//await FT.setContent(target_ctn,content_home);
-		//}
+		default:{
+			const content_home = await demo_home_tpl();
+			await FT.setContent(target_ctn,content_home);
+		}
 		
 	}
 }
