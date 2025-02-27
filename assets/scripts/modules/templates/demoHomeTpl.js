11c0c99d90cb86adb538552bcf65d756ae202a99
aadswebdesign <aad.pouw@gmail.com>
2025-02-27 14:54:07 +0100

    adjusted some styling and added more content to home template

--- assets/scripts/modules/templates/demoHomeTpl.js
+++ assets/scripts/modules/templates/demoHomeTpl.js
@@ -29,48 +29,51 @@ export const demo_home_tpl = async ()=>{
 				<li class='list-disc small-caps'>HTML</li>
 				<li>
 					<ul class='ul-indent'>
-						<li class='list-circle medium-caps'>It's the first thing I learned about a while back!</li>
-						<li class='list-circle medium-caps'>It began with <em class='quoted-single-uc relative'>frameset</em>, then I began using <em class='quoted-single-uc relative'>tables</em> to create a sort of a layout.</li>
-						<li class='list-circle medium-caps'>Today, I know how to setup a html page and more is no need for because there is <b>MDN</b> and that's my <em class='quoted-single-uc relative'>cheat sheet</em> for any more info I need.</li>
+						<li class='list-circle-os medium-caps'>It's the first thing I learned about a while back!</li>
+						<li class='list-circle-os medium-caps'>It began with <em class='quoted-single-uc relative'>frameset</em>, then I began using <em class='quoted-single-uc relative'>tables</em> to create a sort of a layout.</li>
+						<li class='list-circle-os medium-caps'>Today, I know how to setup a html page and more is no need for because there is <b>MDN</b> and that's my <em class='quoted-single-uc relative'>cheat sheet</em> for any more info I need.</li>
 					</ul>
 				</li>
 				<li class='list-disc small-caps'>CSS</li>
 				<li>
 					<ul class='ul-indent'>
-						<li class='list-circle medium-caps'>At my first encounter with css, I was very enthusiast about it. Finally a way to get rid of those tables, to create a layout.</li>
-						<li class='list-circle medium-caps'>Sure, it was new and it had a lot of limitations.The most irritating and time consuming in that time was <em class='quoted-single-uc relative'>float left/right</em>, that was really a hasle.
+						<li class='list-circle-os medium-caps'>At my first encounter with css, I was very enthusiast about it. Finally a way to get rid of those tables, to create a layout.</li>
+						<li class='list-circle-os medium-caps'>Sure, it was new and it had a lot of limitations.The most irritating and time consuming in that time was <em class='quoted-single-uc relative'>float left/right</em>, that was really a hasle.
 						</li>
-						<li class='list-circle medium-caps'>Then <em class='quoted-single-uc relative'>position relative/absolute</em> was introduced and made to ditch the use of floats as much as possible.
+						<li class='list-circle-os medium-caps'>Then <em class='quoted-single-uc relative'>position relative/absolute</em> was introduced and made to ditch the use of floats as much as possible.
 						</li>
-						<li class='list-circle medium-caps'>After that, other great features were introduced like <em class='quoted-single-uc relative'>flex, grid and name it ...</em> and what I use most of this for layout is <em class='quoted-single-uc relative'>flex</em> and <em class='quoted-single-uc relative'>position</em>.</li>
-						<li class='list-circle medium-caps'>Today and same as with html, I know the basics and more is no need for because there is <b>MDN</b> and that's my <em class='quoted-single-uc relative'>cheat sheet</em> for any more info I need.</li>
-					</ul>
-				</li>
-				<li class='list-disc small-caps'>JAVASCRIPT</li>
-				<li>
-					<ul class='ul-indent'>
-						<li class='list-circle medium-caps'>todo</li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
+						<li class='list-circle-os medium-caps'>After that, other great features were introduced like <em class='quoted-single-uc relative'>flex, grid and name it ...</em> and what I use most of this for layout is <em class='quoted-single-uc relative'>flex</em> and <em class='quoted-single-uc relative'>position</em>.</li>
+						<li class='list-circle-os medium-caps'>Today and same as with html, I know the basics and more is no need for because there is <b>MDN</b> and that's my <em class='quoted-single-uc relative'>cheat sheet</em> for any more info I need.</li>
 					</ul>
 				</li>
 				<li class='list-disc small-caps'>PHP</li>
 				<li>
 					<ul class='ul-indent'>
-						<li class='list-circle medium-caps'>todo</li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
+						<li class='list-circle-os medium-caps'><em class='quoted-double-uc relative'>Down the rabbit hole! (php4)</em>, that's where it all began with and where I learned about <em class='quoted-single-uc relative'>arrays, functions and the dollar sign</em></li>
+						<li class='list-circle-os medium-caps'>After that, I discovered <b>zend framework</b> and learned about the <em class='quoted-single-uc relative'><b>M</b>odel<b>V</b>iew<b>C</b>ontroller</em> construct.</li>
+						<li class='list-circle-os medium-caps'>I became quite familiar with it but to a certain level. Long story...!</li>
+						<li class='list-circle-os medium-caps'>Then, someone approached me for a wp website and I never had worked with it and no experience. Still and as I was jobless in that time, I took the job to build a wp site.It didn't go well but not because my lack of knowledge but that customer had an unrealistic idea of building a website. She tought that if you are trowing a bag of plugins on it, it will all be allright! Enough about it!
+						</li>
+						<li class='list-circle-os medium-caps'>Today, 'not this gh page, but the project I'm working on, is running on <b>Php Slim</b>, has an unique structure and I build a lot myself.</li>
+						<li class='list-circle-os medium-caps'></li>
+					</ul>
+				</li>
+				<li class='list-disc small-caps'>JAVASCRIPT</li>
+				<li>
+					<ul class='ul-indent'>
+						<li class='list-circle-os medium-caps'>todo</li>
+						<li class='list-circle-os medium-caps'></li>
+						<li class='list-circle-os medium-caps'></li>
+						<li class='list-circle-os medium-caps'></li>
 					</ul>
 				</li>
 				<li class='list-disc small-caps'>SVG</li>
 				<li>
 					<ul class='ul-indent'>
-						<li class='list-circle medium-caps'>todo</li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
-						<li class='list-circle medium-caps'></li>
+						<li class='list-circle-os medium-caps'>todo</li>
+						<li class='list-circle-os medium-caps'></li>
+						<li class='list-circle-os medium-caps'></li>
+						<li class='list-circle-os medium-caps'></li>
 					</ul>
 				</li>
 			</ul>
