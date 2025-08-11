152d2fbc259bdbf5a85f3373e4ffe4f249c802ed
aadswebdesign <aad.pouw@gmail.com>
2025-08-11 17:11:44 +0200

    updated articles

--- assets/scripts/modules/templates/menubars/menubar_one_tpl.js
+++ assets/scripts/modules/templates/menubars/menubar_one_tpl.js
@@ -6,9 +6,9 @@ const pre_code = `
 (async ()=>{
     /** 
       This is the place 
-	  where all the js logic
-      comes together in 
-	  the right order!
+      where all the js logic
+      ends up but in 
+      the right order!
    */
 })();
 </pre>`;
@@ -16,11 +16,12 @@ const pre_code = `
 		<article class='how-i display-flex relative'>
 			<h2 class='relative'>Loading javascript.</h2>
 			<article-header class='relative'>
-				<h3 class='relative'>Introduction.</h3>
+				<h3 class='relative'>Intro.</h3>
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one'>
 					Recently I read an article about <b>Bulletproof web component loading</b> and written by someone who knows much more about javascript as me.
+					It was about loading in the footer or in the head and using 'async/defer'
 				</p>
 			</article-main><!-- article-main 1-->
 			<article-header class='relative'>
@@ -28,15 +29,16 @@ const pre_code = `
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one relative'>
-					His approach was by using <em>document.readyState</em> and <em>DOMContentLoaded</em> within his component classes.
+					His 'bulletproof' approach was by using <em>document.readyState</em> and <em>DOMContentLoaded</em> in his component classes.
 				</p>
 			</article-main><!-- article-main 2-->
 			<article-header class='relative'>
-				<h3 class='relative'>My opinion.</h3>
+				<h3 class='relative'>My idea.</h3>
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one relative'>
-					My opinion is that there are better ways to do that. A way that is <b>Simple</b>, <b>Bulletproof</b> and <b>Fast</b>
+					It might be bulletproof but complex and hard to maintain
+					The way I do that is <b>Simple</b>, <b>Bulletproof</b>, <b>Fast</b> and hardly needs any maintenance.
 				</p>
 			</article-main><!-- article-main 3-->				
 			<article-header class='relative'>
@@ -51,7 +53,7 @@ const pre_code = `
 						<p class='relative'>
 							First step is to have an <em>index.js</em> within the scripts folder and that contains the following.
 						</p>
-						<code class='relative'>//the imports.</code>
+						<code class='relative'>//the imports on top.</code>
 						${pre_code}							
 					</li>
 					<li class='caps-1 relative'>
@@ -67,8 +69,8 @@ const pre_code = `
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one relative'>
-					At the first step, the code has been placed in an (<b>I</b>mmediately <b>I</b>nvoked <b>F</b>unction <b>E</b>xpression)!
-					This means that the promise is 100% fulfilled before it's passed to your <em>index.html</em>.
+					At the first step, the code has been placed in an async (<b>I</b>mmediately <b>I</b>nvoked <b>F</b>unction <b>E</b>xpression)!
+					That iife, is the final place where my js stuff ends up.
 				</p>
 			</article-main><!-- article-main 5-->				
 			<article-header class='relative'>
@@ -76,21 +78,19 @@ const pre_code = `
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one relative'>
-					It's a completed substance that your <em>index.html</em> receives and as a result of that there is no need of using <em>document.readyState</em> or <em>DOMContentLoaded</em> anywhere in the stack.
+					This means that the promise is 100% fulfilled before it's passed to my <em>index.html</em>.
+					So what I get is a single substance that is complete and ready to use.
+					Actually I rarely look at it, is no need for!				
 				</p>
 			</article-main><!-- article-main 6-->				
 			<article-header class='relative'>
-				<h3 class='relative'>Important!</h3>
+				<h3 class='relative'>Note!</h3>
 			</article-header>
 			<article-main class='display-flex relative'>
 				<p class='fl-one relative'>
-					Placing all in the right order within you <em>index.js</em> matters!
+					Placing all in the right order within you <em>index.js</em> is important!
 				</p>
 			</article-main><!-- article-main 6-->				
-			
-			
-			
-			
 			<article-footer class='relative'>
 				<p class='relative'>
 					More about <b>IIFE</b> at <a href='https://developer.mozilla.org/en-US/docs/Glossary/IIFE' target='_blank' class='relative' >MDN(<b>IIFE</b>)</a>
