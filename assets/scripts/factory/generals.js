0d021b1bb2223866ebf86b839a7debd414287704
aadswebdesign <aad.pouw@gmail.com>
2025-08-12 12:45:06 +0200

    added file generals.js

---
+++ assets/scripts/factory/generals.js
@@ -0,0 +1,13 @@
+/** localhost assets/scripts/factory/generals.js */
+export const generals = async ()=>{
+	(async ()=>{
+		//todo, testing
+		const regex = "/\/[a-zA-Z0-9-._~:/?#\[\]@!$&'()*+,;=%]*)?/g";
+		let expression;
+		if(expression !== undefined){
+			const not_wanted = 'javascript:' + expression.match(regex);
+			if(window.location === not_wanted) return;
+			console.log('not_wanted: ',not_wanted);
+		}
+	})();
+};
