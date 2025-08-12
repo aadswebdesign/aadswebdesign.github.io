29c5f802fc1d550a4315987daf8e2fab2047033f
aadswebdesign <aad.pouw@gmail.com>
2025-08-12 12:47:10 +0200

    implemented generals.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 import {getBaseObjects} from './factory/dom_objects.js';
 import * as DA from './factory/dom_agents.js';
 import * as FT from "./factory/functions.js";
+import {generals} from "./factory/generals.js";
 import {getActions} from './modules/actions/get_actions.js';
 import * as WCP from './../managers/webComponents/components_export.js';
 
@@ -15,5 +16,5 @@ import * as WCP from './../managers/webComponents/components_export.js';
 	await getActions(base_elems);
 	await DA.userAgentSniffer();
 	await DA.userAgentString();
-
-})()
+	await generals();
+})();
