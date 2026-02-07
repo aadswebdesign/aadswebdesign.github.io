554ca40a65aaed30fa320bbe0e5ae4ed79d10236
aadswebdesign <aad.pouw@gmail.com>
2026-02-07 04:24:23 +0100

    unicode issue2

--- assets/scripts/modules/templates/tpl_004.js
+++ assets/scripts/modules/templates/tpl_004.js
@@ -8,16 +8,53 @@ ${TP.artHeaderOpen('relative')}
 ${TP.headingFour('relative',`Android unicode issue`)}
 ${TP.artHeaderEnd()}
 ${TP.artMainOpen('display-flex relative')}
-<p class='trial caret-up-uc relative'>caret-up-uc</p>
-<p class='trial caret-down-uc relative'>caret-down-uc</p>
-<p class='trial caret-left-uc relative'>caret-left-uc</p>
-<p class='trial caret-right-uc relative'>caret-right-uc</p>
-<p class='trial caret-up-small-uc relative'>caret-up-small-uc</p>
-<p class='trial caret-down-small-uc relative'>caret-down-small-uc</p>
-<p class='trial caret-left-small-uc relative'>caret-left-small-uc</p>
-<p class='trial caret-right-small-uc relative'>caret-right-small-uc</p>
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-up-uc relative'>caret-up-uc</summary>
+<details-content class='relative'>caret-up-uc</details-content>
+</details></p>
 
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-down-uc relative'>caret-down-uc</summary>
+<details-content class='relative'>caret-down-uc</details-content>
+</details></p>
 
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-left-uc relative'>caret-left-uc</summary>
+<details-content class='relative'>caret-left-uc</details-content>
+</details></p>
+
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-right-uc relative'>caret-right-uc</summary>
+<details-content class='relative'>caret-right-uc</details-content>
+</details></p>
+
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-up-small-uc relative'>caret-up-small-uc</summary>
+<details-content class='relative'>caret-up-small-uc</details-content>
+</details></p>
+
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-down-small-uc relative'>caret-down-small-uc</summary>
+<details-content class='relative'>caret-down-small-uc</details-content>
+</details></p>
+
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-left-small-uc  relative'>caret-left-small-uc</summary>
+<details-content class='relative'>caret-left-small-uc</details-content>
+</details></p>
+
+<p class='relative'>
+<details class='relative'>
+<summary class='trial caret-right-small-uc relative'>caret-right-small-uc</summary>
+<details-content class='relative'>caret-right-small-uc</details-content>
+</details></p>
 ${TP.artMainEnd()}${TP.artEnd()}
 	`;
 	return tpl;
