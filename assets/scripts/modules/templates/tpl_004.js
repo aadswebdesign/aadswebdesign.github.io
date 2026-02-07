a2a206d66c99b6bae8df6bcf72ef7c30ea55c75a
aadswebdesign <aad.pouw@gmail.com>
2026-02-07 10:29:45 +0100

    emptied template 04 and edited a style rule

--- assets/scripts/modules/templates/tpl_004.js
+++ assets/scripts/modules/templates/tpl_004.js
@@ -5,87 +5,9 @@ export const template_004 = ()=>{
 ${TP.artOpen()}
 ${TP.headingThree('align-center relative',`Just a place to try things out!`)}
 ${TP.artHeaderOpen('relative')}
-${TP.headingFour('relative',`Android unicode issue`)}
+${TP.headingFour('relative',``)}
 ${TP.artHeaderEnd()}
 ${TP.artMainOpen('display-flex relative')}
-<p class='relative'>
-<details class='relative'>
-<summary class='trial triangle-up-uc relative'>triangle-up-uc</summary>
-<details-content class='relative'>triangle-up-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial triangle-down-uc relative'>triangle-down-uc</summary>
-<details-content class='relative'>triangle-down-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial triangle-left-uc relative'>triangle-left-uc</summary>
-<details-content class='relative'>triangle-left-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial triangle-right-uc relative'>triangle-right-uc</summary>
-<details-content class='relative'>triangle-right-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-up-uc relative'>caret-up-uc</summary>
-<details-content class='relative'>caret-up-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-down-uc relative'>caret-down-uc</summary>
-<details-content class='relative'>caret-down-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-left-uc relative'>caret-left-uc</summary>
-<details-content class='relative'>caret-left-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-right-uc relative'>caret-right-uc</summary>
-<details-content class='relative'>caret-right-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-up-small-uc relative'>caret-up-small-uc</summary>
-<details-content class='relative'>caret-up-small-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-down-small-uc relative'>caret-down-small-uc</summary>
-<details-content class='relative'>caret-down-small-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-left-small-uc  relative'>caret-left-small-uc</summary>
-<details-content class='relative'>caret-left-small-uc</details-content>
-</details></p>
-
-<p class='relative'>
-<details class='relative'>
-<summary class='trial caret-right-small-uc relative'>caret-right-small-uc</summary>
-<details-content class='relative'>caret-right-small-uc</details-content>
-</details></p>
-
-
-
-
-
-
-
 
 ${TP.artMainEnd()}${TP.artEnd()}
 	`;
