94a5c423e883c74d6f2f65d3bfb349cb996408b0
aadswebdesign <aad.pouw@gmail.com>
2026-04-03 18:52:05 +0200

    eliminated all documentQueries, now this site is working directly with the dom

--- assets/scripts/factory/get_dom_objects.js
+++ assets/scripts/factory/get_dom_objects.js
@@ -1,8 +1,8 @@
 /** assets/scripts/factory/get_dom_objects.js */
 import * as FT from './functions.js';//getIds
 export async function getDomObjects(){
-	const _body = document.body;
-	return await FT.createObjects('base_objects',{
+	const body_elem = document.body;
+	const get_objects = await FT.createObjects('base_objects',{
 		vvp: window.visualViewport,
 		location_data:{
 			loc_full: window.location,
@@ -10,28 +10,40 @@ export async function getDomObjects(){
 			loc_origin: window.location.origin, 
 			loc_path: location.pathname,
 		},
-		body: _body,
-		wrap_ctn: _body.firstElementChild ?? null,
-		top_ctn: await FT.elQuery('[data-ctn_id = "ctn_top"]', false,self.wrap_ctn),
-		main_elem: await FT.elQuery('main', false, self.wrap_ctn), 
-		workbench_ctn: await FT.elQuery('main-content.workbench-ctn',false,self.main_elem),
-		
-		
+		body: body_elem,
 	});
+	const {body} = get_objects;
+	get_objects.wrap_ctn = body.firstElementChild;
+	const {wrap_ctn} = get_objects;
+	if(wrap_ctn.children.length > 0){
+		const wrap_children = wrap_ctn.children;
+		get_objects.top_ctn = wrap_children[0];
+		get_objects.main_elem = wrap_children[1];
+	}
+	const {top_ctn,main_elem} = get_objects;
+	get_objects.workbench_ctn = main_elem.firstElementChild;
+	get_objects.controlls_ctn = main_elem.lastElementChild;
+	console.log('top_ctn: ',top_ctn);
+	console.log('main_elem: ', main_elem);
+	return get_objects;
 }
 export async function getDomObjectsExtended(obj_args){
-	const {vvp,location_data,body,wrap_ctn,top_ctn,main_elem,workbench_ctn} = obj_args;
-	return await FT.createObjects('extended_objects',{
+	const {vvp,location_data,body,wrap_ctn,top_ctn,main_elem,workbench_ctn,controlls_ctn} = obj_args;
+	const get_objects = await FT.createObjects('extended_objects',{
 		vvp,body,main_elem,workbench_ctn,top_ctn,location_data,
 		active_elem: document.activeElement,
-		wb_content: await FT.elQuery('content-item.wb-content', false, self.workbench_ctn),		
-		controlls_ctn: await FT.elQuery('div.controls-ctn',false,self.main_elem),
-		ctn_top : await FT.elQuery('aside.sidebar-ctrl.top',false, self.controlls_ctn ),
-		ctn_left: await FT.elQuery('aside.sidebar-ctrl.left',false, self.controlls_ctn ),
-		open_close_block: await FT.elQuery('div.open-close-block',false,self.controlls_ctn),
-		toggles:{
-			tb_toggle: await FT.getId('toolbar_toggle'),
-			mb_toggle: await FT.getId('menubar_toggle'),
-		},
-	});
+	});	
+	get_objects.wb_content = workbench_ctn.firstElementChild;
+	if(controlls_ctn.children.length > 0){
+		const controls = controlls_ctn.children;
+		get_objects.ctn_top = controls[0];
+		get_objects.open_close_block = controls[1]; 
+		get_objects.ctn_left = controls[2];  
+	}
+	const	{open_close_block} = get_objects;
+	get_objects.toggles = {
+			tb_toggle: open_close_block.firstElementChild, //toolbar_toggle
+			mb_toggle: open_close_block.lastElementChild,	//menubar_toggle
+	};		
+	return get_objects;
 }
