791156e5a40044f8aba70eb1e1343888f7e11057
aadswebdesign <aad.pouw@gmail.com>
2026-06-25 12:58:02 +0200

    updated action_off_cs.js

--- assets/moduleEditor/scripts/editor_logic/actions/constructs/action_off_cs.js
+++ assets/moduleEditor/scripts/editor_logic/actions/constructs/action_off_cs.js
@@ -10,13 +10,47 @@ class ActionOffCs{
 		this.br_el = MDFT.brNode.cloneNode();
 		(async()=> {
 			if(this.#parent_el !== null){
-				if(parent_el.lastElementChild !== null){
-					this.last_child = parent_el.lastElementChild;
-					this.ancestor = await MFT.getAncestor(this.last_child,this.#parent_el,this.#tag_name);				
-					MDFT.appendBr(this.ancestor,this.br_el);
-					MDFT.removeBlockActive(this.ancestor,'data-block_active');		
-					MDFT.removeLastBr(this.ancestor,this.br_el);
-				}
+				(async()=> {
+					if(this.#parent_el.firstElementChild !== null){
+						this.first_child = this.#parent_el.firstElementChild;
+						this.new_child = this.first_child;
+						const children = await MFT.getTagNames('*',this.new_child);
+						for(const child of children){
+							if(child.tagName !== 'ARTICLE-HEADER' && child.tagName !== 'ARTICLE-MAIN' && child.tagName !== 'ARTICLE-FOOTER'){
+								if(child.hasAttribute('data-block_active')){
+									child.removeAttribute('data-block_active');
+								}
+								
+								
+							}
+						}
+					}
+				})();	
+				(async()=> {
+					if(parent_el.lastElementChild !== null){
+						this.last_child = parent_el.lastElementChild;
+						this.ancestor = await MFT.getAncestor(this.last_child,this.#parent_el,this.#tag_name);				
+						MDFT.appendBr(this.ancestor,this.br_el);
+						MDFT.removeBlockActive(this.ancestor,'data-block_active');		
+						MDFT.removeLastBr(this.ancestor,this.br_el);
+					}
+				})();
+				(async()=> {
+					const grand = this.#parent_el.parentElement.parentElement;
+					const btn_blocks = await MFT.getTagNames('BTN-BLOCK',grand);
+					let block;
+					for(const btn_block of btn_blocks){
+						const ds = btn_block.dataset;
+						if(ds.grpName !== 'article-grp' && ds.grpName !== 'heading-grp'){
+							block = btn_block;
+							if(block !== undefined){
+								if(block.hasAttribute('data-on')){
+									block.removeAttribute('data-on');
+								}
+							}//mdlName = 'h3_mdl'
+						}
+					}	
+				})();					
 			}	
 		})();
 	}
